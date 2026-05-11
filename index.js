075d63a442c805b52ea3cdbd972130088105cecc
WONG JI KIN <25140435@sunwayedu.local>
2026-05-11 10:37:52 +0800

    added timetable formatting

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const clockElement = document.getElementById("clockElement");
+const timePeriodElement = document.getElementById("timePeriodElement");
+const monElement = document.getElementById("monElement");
+const tuesElement = document.getElementById("tuesElement");
+const wedElement = document.getElementById("wedElement");
+const thursElement = document.getElementById("thursElement");
+const friElement = document.getElementById("friElement");
+
 
 const updateRate = 500;
 
@@ -6,22 +13,34 @@ const updateRate = 500;
 
 function getTimeString() {
     const date = new Date();
-    const hourString = date.getHours().toString()
-    const minString = date.getMinutes().toString().padStart(2, "0")
-    const secString = date.getSeconds().toString().padStart(2, "0")
+    const hourString = date.getHours().toString();
+    const minString = date.getMinutes().toString().padStart(2, "0");
+    const secString = date.getSeconds().toString().padStart(2, "0");
     
     
     return (
         hourString + ":" +
         minString + ":" +
         secString
-    )
-}
+    );
+};
+
+function initialTimetableFormat() {
+    const elements = [timePeriodElement, monElement, tuesElement, wedElement, thursElement, friElement];
+
+    for (let i = 0; i < 6; i++) {
+        elements[i].textContent = i;
+        elements[i].style.width = "1000px"
+    };
+};
+
+
+initialTimetableFormat();
 
 
 (function update() {
     setTimeout(() => {
-        clockElement.textContent = getTimeString()
+        clockElement.textContent = getTimeString();
 
         update();
     }, 500);
