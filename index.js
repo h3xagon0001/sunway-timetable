a6c2de6dfa449ac8c2a32380a0bb5c66f15e7fa7
WONG JI KIN <25140435@sunwayedu.local>
2026-05-13 09:55:19 +0800

    added working functionality to position elements absolutely in each cell

--- index.js
+++ index.js
@@ -25,18 +25,31 @@ function getTimeString() {
     );
 };
 
+function numToPixel(num) {
+    return num.toString() + "px";
+};
+
 function initialTimetableFormat() {
     const elements = [timePeriodElement, monElement, tuesElement, wedElement, thursElement, friElement];
 
     for (let i = 0; i < 6; i++) {
-        elements[i].textContent = i;
-        elements[i].style.width = "1000px"
+        elements[i].style.width = "500px"
+        if (elements[i] == timePeriodElement) { elements[i].style.height = "50px" }
+        else { elements[i].style.height = "100px" };
     };
 };
 
+function addClassElement(day, subject) {
+    const classElement = document.createDocumentFragment()
+        .appendChild(document.createElement("div"));
+    classElement.textContent = subject
+
+    document.getElementById(day).appendChild(classElement)
+}
+
 
 initialTimetableFormat();
-
+addClassElement("monElement", "dsad")
 
 (function update() {
     setTimeout(() => {
